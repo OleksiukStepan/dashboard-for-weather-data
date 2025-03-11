ad0d3b6bfc465d4bd9760ed9d44ef2d0efa6ddb2
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-03-11 16:05:18 +0200

    add list of cities

---
+++ apps/weather/cities.py
@@ -0,0 +1,7 @@
+CITIES = [
+    "New York", "London", "Kyiv", "Prague", "Paris", "Sydney", "Toronto", "Berlin", "Rome", "Madrid",
+    "Los Angeles", "Chicago", "Houston", "Miami", "San Francisco", "Moscow", "Tokyo", "Beijing",
+    "Delhi", "Jakarta", "Istanbul", "Mexico City", "Seoul", "Bangkok", "Buenos Aires", "Cairo",
+    "Shanghai", "Lagos", "Rio de Janeiro", "Lima", "Mumbai", "Osaka", "Karachi", "Shenzhen",
+    "Guangzhou", "Chengdu", "Tehran", "Sao Paulo", "Bogota", "Ho Chi Minh City", "Johannesburg"
+]
