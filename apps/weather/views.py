6677aa3f417fb246778a84dea2faed1da5474f01
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-03-11 16:50:34 +0200

    add views

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from datetime import timedelta
 
-# Create your views here.
+from django.shortcuts import render
+from django.utils.timezone import now
+from rest_framework.generics import ListCreateAPIView, ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from apps.weather.cities import CITIES
+from apps.weather.models import City, WeatherData
+from apps.weather.serializers import CitySerializer, WeatherDataSerializer
+
+
+def index(request):
+    return render(request, "index.html", {"cities": CITIES})
+
+
+class CityListView(ListCreateAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class WeatherDataListView(ListAPIView):
+    queryset = WeatherData.objects.all()
+    serializer_class = WeatherDataSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["city__name", "date"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        city_name = self.request.GET.get("city__name")
+        days = self.request.GET.get("days")
+
+        if city_name:
+            queryset = queryset.filter(city__name=city_name)
+
+        if days:
+            days = int(days)
+            start_date = now().date() - timedelta(days=days)
+            queryset = queryset.filter(date__gte=start_date)
+
+        return queryset
